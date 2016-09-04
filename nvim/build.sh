a4d39c96fa91c3fb81bf354123103d38fb03ed0b
AGhost-7 <jonathan.boudreau.92@gmail.com>
2016-09-04 03:16:07 +0000

    Tweaks, hopefully automated build works...

--- nvim/build.sh
+++ nvim/build.sh
@@ -10,7 +10,7 @@ apt-install () {
 	sudo apt-get install -y --no-install-recommends "$@"
 }
 
-apt-install python-pip python-dev software-properties-common -y
+apt-install build-essential python-dev software-properties-common -y
 
 # Fix the permissions from the copy...
 sudo chown -R aghost-7:aghost-7 /home/aghost-7/.config/nvim
@@ -32,17 +32,15 @@ sudo rm /tmp/bashrc-additions.sh
 curl -fLo ~/.config/nvim/autoload/plug.vim --create-dirs \
 	https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
 
-
 # Install all plugins.
 nvim +PlugInstall +qall
 
 # Install YouCompleteMe
-apt-install cmake
+apt-install cmake gcc
 /home/aghost-7/.config/nvim/plugged/YouCompleteMe/install.py
 
 # Shellcheck: Grade A shell script linter
 git clone https://github.com/koalaman/shellcheck.git /tmp/shellcheck
-	sudo apt-get install haskell-platform -y 
 apt-install haskell-platform
 cabal update
 cd /tmp/shellcheck
@@ -50,8 +48,7 @@ cabal install --force-reinstalls
 sudo cp ~/.cabal/bin/shellcheck /usr/local/bin/shellcheck
 
 # Cleanups
-sudo apt-get purge software-properties-common cmake haskell-platform -y
-sudo apt-get autoremove -y
+sudo apt-get purge build-essential software-properties-common cmake haskell-platform -y
 sudo apt-get clean
 rm -rf /tmp/shellcheck
 rm -rf ~/.cabal
