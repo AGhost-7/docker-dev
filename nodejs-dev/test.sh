d5ea709ae8aede10848dd935cf050713ac7024cb
CExJBoudreau <jboudreau@i-sight.com>
2016-09-20 20:27:35 +0000

    Hotfix

--- nodejs-dev/test.sh
+++ nodejs-dev/test.sh
@@ -8,6 +8,8 @@ testnode() {
 	docker build -t "aghost7/nodejs-dev:$1" --build-arg NODE_VERSION="$1" .
 	docker run --rm -ti "aghost7/nodejs-dev:$1" \
 		bash -c ". /home/aghost-7/.nvm/nvm.sh && node -p '1 + 1'"
+	docker run --rm "aghost7/nodejs-dev:$1" \
+		bash -c ". /home/aghost-7/.nvm/nvm.sh && node -v"
 }
 
 testnode v0.10.38
