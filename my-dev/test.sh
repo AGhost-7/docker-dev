f89b05ddc7a2f49ef100bbe6d169beebd5d8cf80
AGhost-7 <jonathan.boudreau.92@gmail.com>
2016-10-26 21:02:44 -0400

    Mysql builds with correct config now.

--- my-dev/test.sh
+++ my-dev/test.sh
@@ -1,4 +1,13 @@
 #!/usr/bin/env bash
 
+set -e
+
 docker build -t aghost7/my-dev:5.6 .
 
+docker run -d --name my-dev aghost7/my-dev:5.6
+
+sleep 5
+
+docker exec -ti my-dev mysql -u root 'select 1'
+docker kill my-dev
+docker rm my-dev
