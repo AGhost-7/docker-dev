9a7ee41c28e6c6f7d377f03776147c150c2d48e8
AGhost-7 <jonathan.boudreau.92@gmail.com>
2016-10-26 20:45:37 -0400

    Remove ports...

--- pg-dev/test.sh
+++ pg-dev/test.sh
@@ -4,16 +4,11 @@ set -e
 
 docker build -t aghost7/pg-dev:9.3 .
 
-docker run --name pg-dev -d -p 5432:5432 aghost7/pg-dev:9.3
+docker run --name pg-dev -d aghost7/pg-dev:9.3
 
 sleep 5
 
-if [ "$(which psql)" == "" ]; then
-	sudo apt-get update
-	sudo apt-get install postgresql-client -y
-fi
-
-psql template1 -U postgres
+docker exec pg-dev psql template1 -c 'select 1'
 
 docker kill pg-dev
 docker rm pg-dev
