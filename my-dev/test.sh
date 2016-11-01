a699d54cdbfd85b42d8a8ee0d9144bdfde32d7c2
AGhost-7 <jonathan.boudreau.92@gmail.com>
2016-10-31 21:57:52 -0400

    Fix mysql test

--- my-dev/test.sh
+++ my-dev/test.sh
@@ -4,10 +4,10 @@ set -e
 
 docker build -t aghost7/my-dev:5.6 .
 
-docker run -d --name my-dev aghost7/my-dev:5.6
+docker run -e MYSQL_ALLOW_EMPTY_PASSWORD=1 -d --name my-dev aghost7/my-dev:5.6
 
 sleep 5
 
-docker exec -ti my-dev mysql -u root 'select 1'
+docker exec -ti my-dev mysql -u root -e 'select 1'
 docker kill my-dev
 docker rm my-dev
