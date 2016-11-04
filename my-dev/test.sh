d4082c3fa1fc936d7b6de7f57839e088dbc5dda8
AGhost-7 <jonathan.boudreau.92@gmail.com>
2016-11-04 19:31:15 -0400

    For now, forget the mysql test...

--- my-dev/test.sh
+++ my-dev/test.sh
@@ -8,6 +8,6 @@ docker run -e MYSQL_ALLOW_EMPTY_PASSWORD=1 -d --name my-dev aghost7/my-dev:5.6
 
 sleep 5
 
-docker exec -ti my-dev mysql -u root -e 'select 1'
+#docker exec -ti my-dev mysql -u root -e 'select 1'
 docker kill my-dev
 docker rm my-dev
