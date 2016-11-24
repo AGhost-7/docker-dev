690b6d9faf0e6a7f85d53b4d424ae12e449586a7
AGhost-7 <jonathan.boudreau.92@gmail.com>
2016-11-23 19:40:09 -0500

    update tests for nodejs-dev

--- nodejs-dev/test.sh
+++ nodejs-dev/test.sh
@@ -5,7 +5,8 @@ set -e
 set -x
 
 testnode() {
-	docker build -t "aghost7/nodejs-dev:$1" --build-arg NODE_VERSION="$1" .
+	docker build -t "aghost7/nodejs-dev:$1" "$1"
+
 	docker run --rm -ti "aghost7/nodejs-dev:$1" \
 		bash -c ". /home/aghost-7/.nvm/nvm.sh && node -p '1 + 1'"
 	docker run --rm "aghost7/nodejs-dev:$1" \
@@ -13,4 +14,5 @@ testnode() {
 }
 
 testnode v0.10.38
-testnode v4.5.0
+testnode argon
+testnode boron
