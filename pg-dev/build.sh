a10fe9419ab6f682e5b450a3062c366f3636fddb
AGhost-7 <jonathan.boudreau.92@gmail.com>
2017-01-21 13:12:21 -0500

    Update to use pgcli fork so I dont have to use \x all the time...

--- pg-dev/build.sh
+++ pg-dev/build.sh
@@ -3,10 +3,16 @@
 set -e
 
 apt-get update 
-apt-get install build-essential python-pip python-dev postgresql-server-dev-9.3 -y
+apt-get install build-essential python-pip python-dev postgresql-server-dev-9.3 git -y
 
-pip install pgcli
+# Install pgcli through my fork to have the configurable auto_expand.
+git clone https://github.com/AGhost-7/pgcli.git /tmp/pgcli
+cd /tmp/pgcli
+pip install .
+cd /
+rm -rf /tmp/pgcli
 
-apt-get purge python-dev postgresql-server-dev-9.3 build-essential -y
+apt-get purge python-dev postgresql-server-dev-9.3 build-essential git -y
 apt-get autoremove -y
 apt-get clean
+rm -rf /var/lib/apt/lists/*
