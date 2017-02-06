b2714ae8fc0f3f68c8c8af2aa64f98bcf6a092ac
AGhost-7 <jonathan.boudreau.92@gmail.com>
2017-02-05 20:37:24 -0500

    Use own mycli fork for now

--- my-dev/build.sh
+++ my-dev/build.sh
@@ -4,10 +4,11 @@ apt-get update
 
 apt-get install --no-install-recommends curl python-pip python-dev build-essential less -y
 
-curl -L --create-dirs -o /root/.config/mycli \
-    https://raw.githubusercontent.com/AGhost-7/docker-dev/master/pg-dev/pgcliconfig
+git clone https://github.com/AGhost-7/mycli.git /tmp/mycli
 
-pip install mycli
+cp /tmp/mycli
+pip install .
+rm -rf /tmp/mycli
 
 apt-get purge build-essential python-dev -y
 rm -rf /var/lib/apt/lists/*
