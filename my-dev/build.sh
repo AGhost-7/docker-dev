a0c2c14d4af411ddc5a24533793e5db391de8357
AGhost-7 <jonathan.boudreau.92@gmail.com>
2017-02-06 22:18:15 -0500

    Fix issues with silent failure

--- my-dev/build.sh
+++ my-dev/build.sh
@@ -1,14 +1,17 @@
 #!/usr/bin/env bash
 
+set -e
+
 apt-get update
 
-apt-get install --no-install-recommends curl python-pip python-dev build-essential less -y
+apt-get install --no-install-recommends curl python-pip python-dev build-essential less git -y
 
 git clone https://github.com/AGhost-7/mycli.git /tmp/mycli
 
-cp /tmp/mycli
+cd /tmp/mycli
 pip install .
+cd ~/
 rm -rf /tmp/mycli
 
-apt-get purge build-essential python-dev -y
+apt-get purge build-essential python-dev git -y
 rm -rf /var/lib/apt/lists/*
