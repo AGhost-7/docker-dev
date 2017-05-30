ea4d29d443515e204c277827b51067e9cc5470fd
AGhost-7 <jonathan.boudreau.92@gmail.com>
2017-05-30 23:22:01 +0000

    Add proper utf8 encoding to mysql

--- my-dev/build.sh
+++ my-dev/build.sh
@@ -8,5 +8,18 @@ apt-get install --no-install-recommends curl python-pip python-dev build-essenti
 
 pip install mycli
 
+cat > /etc/mysql/conf.d/dev.cnf <<CONFIG
+[client]
+default-character-set = utf8mb4
+
+[mysql]
+default-character-set = utf8mb4
+
+[mysqld]
+character-set-client-handshake = FALSE
+character-set-server = utf8mb4
+collation-server = utf8mb4_unicode_ci
+CONFIG
+
 apt-get purge build-essential python-dev -y
 rm -rf /var/lib/apt/lists/*
