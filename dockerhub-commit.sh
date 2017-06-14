fe2a76b06c7253aace928ff8c4a458bfece4d47d
AGhost-7 <jonathan.boudreau.92@gmail.com>
2017-06-14 21:12:22 +0000

    Update build script to allow pushing branches

--- dockerhub-commit.sh
+++ dockerhub-commit.sh
@@ -1,5 +1,7 @@
 #!/usr/bin/env bash
 
+set -e
+
 # This script updates all of the build branches for dockerhub. Decided to do this since I want to
 # keep everything in a single repository but didn't want to deal with the hassle of rebuilding all
 # images every time I update just a single dockerfile.
@@ -10,13 +12,18 @@ if [ ! -d .build ]; then
 fi
 
 cd .build
-for d in $(find . -maxdepth 1 -not -name '.*' -and -type d); do
+find .. -maxdepth 1 -not -name '.*' -and -type d | while read d; do
 	image_name="$(basename $d)"
+	echo "Checking image $image_name"
 	branch="dockerhub/$image_name"
 	git checkout "$branch" 2> /dev/null || (git checkout --orphan "$branch" && git reset --hard)
-	cp -r "../$d/"* .
+	cp -r "../$image_name/"* .
 	git add -A
 	if ! git diff-index --quiet HEAD --; then
 		git commit -m "Update branch for image $image_name"
 	fi
+	if [ "$1" == "--push" ]; then
+		git push origin "branch"
+	fi
 done
+
