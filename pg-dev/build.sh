a86142cffe092ed88820dd0ea493a7142122fda4
AGhost-7 <jonathan.boudreau.92@gmail.com>
2017-09-19 23:03:41 +0000

    Move off of fork

--- pg-dev/build.sh
+++ pg-dev/build.sh
@@ -5,12 +5,7 @@ set -e
 apt-get update 
 apt-get install build-essential python-pip python-dev postgresql-server-dev-9.3 git -y
 
-# Install pgcli through my fork to have the configurable auto_expand.
-git clone https://github.com/AGhost-7/pgcli.git /tmp/pgcli
-cd /tmp/pgcli
-pip install .
-cd /
-rm -rf /tmp/pgcli
+pip install pgcli
 
 apt-get purge python-dev postgresql-server-dev-9.3 build-essential git -y
 apt-get autoremove -y
