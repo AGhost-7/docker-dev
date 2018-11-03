e1f834850ce552aaed88cd39b6acfd8685fab3f2
AGhost-7 <jonathan.boudreau.92@gmail.com>
2018-11-03 14:55:47 -0400

    Trigger build of c-dev

--- images/c-dev/build.sh
+++ images/c-dev/build.sh
@@ -14,9 +14,11 @@ cd build
 cmake .. -DCMAKE_BUILD_TYPE=Release -DCMAKE_INSTALL_PREFIX=/usr/local -DCMAKE_EXPORT_COMPILE_COMMANDS=YES
 cmake --build .
 sudo cmake --build . --target install
+# We'll probably want this later.
 sudo cp bin/clang /usr/local/bin
 cd /
 
+# Add plugins and customizations
 cat /tmp/post-plugin.vim >> ~/.config/nvim/post-plugin.vim
 cat /tmp/plugin.vim >> ~/.config/nvim/plugin.vim
 nvim +PlugInstall +qall
