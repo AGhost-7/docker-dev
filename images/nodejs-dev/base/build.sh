669b91861146047149874061d28399710cadf836
AGhost-7 <jonathan.boudreau.92@gmail.com>
2019-01-20 11:37:07 -0500

    Fix issue with yarn installation

--- images/nodejs-dev/base/build.sh
+++ images/nodejs-dev/base/build.sh
@@ -12,6 +12,13 @@ done
 # Install node version manager
 curl -o- https://raw.githubusercontent.com/AGhost-7/nvm/v0.33.11/install.sh | bash
 
+# Install yarn without nodejs. The package being at the system-level means it
+# will still be available if you switch node version.
+curl -sS https://dl.yarnpkg.com/debian/pubkey.gpg | sudo apt-key add -
+echo "deb https://dl.yarnpkg.com/debian/ stable main" | sudo tee /etc/apt/sources.list.d/yarn.list
+sudo apt-get update
+sudo apt-get install --no-install-recommends -y yarn
+
 # customize fzf to ignore node_modules
 cat /tmp/bashrc-additions.sh >> ~/.bashrc
 sudo rm /tmp/bashrc-additions.sh
