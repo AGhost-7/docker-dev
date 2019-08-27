cfb81045b41e5c9103c21eec86e3dafe36898366
AGhost-7 <jonathan.boudreau.92@gmail.com>
2019-08-27 17:55:03 +0000

    add more test coverage

--- images/pg-dev/test/sh/test.sh
+++ images/pg-dev/test/sh/test.sh
@@ -1,11 +1,16 @@
 
-docker build -t aghost7/pg-dev:9.3 .
+for version in 9.3 10 11; do
 
-docker run --name pg-dev -d aghost7/pg-dev:9.3
+	docker build -t aghost7/pg-dev:"$version" .
 
-sleep 5
+	docker run --name pg-dev -d aghost7/pg-dev:"$version"
 
-docker exec pg-dev psql -U postgres template1 -c 'select 1'
+	sleep 5
 
-docker kill pg-dev
-docker rm pg-dev
+	docker exec pg-dev psql -U postgres template1 -c 'select 1'
+
+	docker exec -ti pg-dev bash -c 'which pgcli'
+
+	docker kill pg-dev
+	docker rm pg-dev
+done
