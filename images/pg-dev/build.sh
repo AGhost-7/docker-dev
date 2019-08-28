bd1005b949ecf55be3558f94db73b6bebe104a4e
AGhost-7 <jonathan.boudreau.92@gmail.com>
2019-08-27 21:23:33 -0400

    fix build failure

--- images/pg-dev/build.sh
+++ images/pg-dev/build.sh
@@ -2,19 +2,17 @@
 
 set -e
 
-pg_version="$1"
-
 apt-get update 
 apt-get install -y \
 	build-essential \
 	python-pip \
 	python-dev \
-	postgresql-server-dev-"$pg_version" \
+	postgresql-server-dev-9.6 \
 	git
 
 pip install pgcli
 
-apt-get purge python-dev postgresql-server-dev-"$pg_version" build-essential git -y
+apt-get purge python-dev postgresql-server-dev-9.6 build-essential git -y
 apt-get autoremove -y
 apt-get clean
 rm -rf /var/lib/apt/lists/*
