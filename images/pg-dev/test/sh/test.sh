5ff0d80f986f5b8529ec70474d43b27541f3c272
AGhost-7 <jonathan.boudreau.92@gmail.com>
2020-02-18 22:48:18 -0500

    update tests

--- images/pg-dev/test/sh/test.sh
+++ images/pg-dev/test/sh/test.sh
@@ -1,15 +1,13 @@
 
-for version in 9.3 10 11; do
-
-	docker build -t aghost7/pg-dev:"$version" .
+for version in 9.6 10 11 12; do
 
 	docker run --name pg-dev -d aghost7/pg-dev:"$version"
 
-	sleep 5
+	sleep 10
 
 	docker exec pg-dev psql -U postgres template1 -c 'select 1'
 
-	docker exec -ti pg-dev bash -c 'which pgcli'
+	docker exec pg-dev bash -c 'which pgcli'
 
 	docker kill pg-dev
 	docker rm pg-dev
