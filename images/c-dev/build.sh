b7730834e99c28a910ee5a4756eabe20202d1c97
AGhost-7 <jonathan.boudreau.92@gmail.com>
2020-03-06 21:35:33 +0000

    use clangd instead

--- images/c-dev/build.sh
+++ images/c-dev/build.sh
@@ -4,18 +4,7 @@ set -ex
 
 sudo apt-get update
 sudo apt-get install -y cmake gdb
-sudo apt-get install -y --no-install-recommends valgrind
-
-# Download latest clang and build cquery completion engine.
-git clone https://github.com/cquery-project/cquery.git --recursive /tmp/cquery
-cd /tmp/cquery
-git submodule update --init
-mkdir build
-cd build
-cmake .. -DCMAKE_BUILD_TYPE=Release -DCMAKE_INSTALL_PREFIX=/usr/local -DCMAKE_EXPORT_COMPILE_COMMANDS=YES
-cmake --build .
-sudo cmake --build . --target install
-cd /
+sudo apt-get install -y --no-install-recommends valgrind clang-tools clangd
 
 # Add plugins and customizations.
 cat /tmp/post-plugin.vim >> ~/.config/nvim/post-plugin.vim
@@ -23,7 +12,6 @@ cat /tmp/plugin.vim >> ~/.config/nvim/plugin.vim
 nvim +PlugInstall +qall
 
 # Clean up.
-sudo rm -rf /tmp/cquery
 sudo rm /tmp/post-plugin.vim
 sudo rm /tmp/plugin.vim
 sudo rm -rf /var/lib/apt/lists/*
--- images/c-dev/post-plugin.vim
+++ images/c-dev/post-plugin.vim
@@ -1,15 +1,14 @@
 
 let g:LanguageClient_serverCommands = {
-\ 'c': ['/usr/local/bin/cquery',
-\ '--log-file=/tmp/cq.log',
-\ '--init={"cacheDirectory":"' . $HOME . '/.cache/cquery"}']
-\ }
+			\ 'c': ['clangd'],
+			\ 'cpp': ['clangd'],
+			\ }
 
 au FileType c nnoremap <silent> <buffer> K :call LanguageClient#textDocument_hover()<CR>
 au FileType c nnoremap <silent> <buffer> gd :call LanguageClient#textDocument_definition()<CR>
 
 call deoplete#custom#source('LanguageClient',
-	\ 'min_pattern_length',
-	\ 2)
+			\ 'min_pattern_length',
+			\ 2)
 
 let g:deoplete#enable_at_startup = 1
