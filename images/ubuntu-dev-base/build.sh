04e6963055b0817226b102f087f73684a395e1a0
AGhost-7 <jonathan.boudreau.92@gmail.com>
2020-10-23 10:03:18 -0400

    bump

--- images/ubuntu-dev-base/build.sh
+++ images/ubuntu-dev-base/build.sh
@@ -70,6 +70,7 @@ if [ "$UBUNTU_RELEASE" = "bionic" ]; then
 		-o /etc/bash_completion.d/docker-compose
 else
 	. /etc/os-release
+
 	# install podman / buildah on newer releases
 	echo "deb https://download.opensuse.org/repositories/devel:/kubic:/libcontainers:/stable/xUbuntu_${VERSION_ID}/ /" > /etc/apt/sources.list.d/devel:kubic:libcontainers:stable.list
 	curl -L https://download.opensuse.org/repositories/devel:/kubic:/libcontainers:/stable/xUbuntu_${VERSION_ID}/Release.key | sudo apt-key add -
